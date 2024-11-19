b938dfbf36ec0cde7003044cfb72f593c52adf7d
rush17m <rushali17mohbe@gmail.com>
2024-11-18 23:00:02 -0500

    Adding chatbot project

---
+++ tenant-ai-chatbot/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Chatbot from '@/components/Chatbot';
+
+function App() {
+
+  return (
+    <div className='flex flex-col min-h-full w-full max-w-3xl mx-auto px-4'>
+      <header className='sticky top-0 shrink-0 z-20 bg-white'>
+        <div className='flex flex-col h-full w-full gap-1 pt-4 pb-2'>
+          <h1 className='font-urbanist text-[1.65rem] font-semibold'>Tenant.AI Chatbot</h1>
+        </div>
+      </header>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
